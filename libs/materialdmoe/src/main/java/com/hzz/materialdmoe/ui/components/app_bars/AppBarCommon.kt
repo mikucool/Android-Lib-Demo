d024f2932b6521841768a4bcad53d9164b4f7b28
mikucool <q1548701641@gmail.com>
2024-09-17 21:03:10 +0800

    update MaterialDemo: rename UiState.SmallTopBarViewModel -> TopAppBarUiState

--- libs/materialdmoe/src/main/java/com/hzz/materialdmoe/ui/components/app_bars/AppBarCommon.kt
+++ libs/materialdmoe/src/main/java/com/hzz/materialdmoe/ui/components/app_bars/AppBarCommon.kt
@@ -1,6 +1,18 @@
 package com.hzz.materialdmoe.ui.components.app_bars
 
+import androidx.compose.ui.geometry.Rect
+
 
 enum class TopAppScrollBehavior {
     None, EnterAlwaysCollapsed, ExitUntilCollapsed, Pinned
 }
+
+
+data class TopAppBarUiState(
+    val appBarTitle: String = "TITLE SAMPLE",
+    val isShowNavigationIcon: Boolean = false,
+    val isShowBarEndActions: Boolean = false,
+    val appBarScrollBehavior: TopAppScrollBehavior = TopAppScrollBehavior.None,
+    val windowInsetsRect: Rect = Rect(0f, 0f, 0f, 0f),
+    val isUseCustomWindowInsets: Boolean = false,
+)
--- libs/materialdmoe/src/main/java/com/hzz/materialdmoe/ui/components/app_bars/top/SmallTopBarViewModel.kt
+++ libs/materialdmoe/src/main/java/com/hzz/materialdmoe/ui/components/app_bars/top/SmallTopBarViewModel.kt
@@ -3,14 +3,15 @@ package com.hzz.materialdmoe.ui.components.app_bars.top
 import androidx.compose.ui.geometry.Rect
 import androidx.lifecycle.ViewModel
 import com.hzz.materialdmoe.data.MaterialDemoLocalRepository
+import com.hzz.materialdmoe.ui.components.app_bars.TopAppBarUiState
 import com.hzz.materialdmoe.ui.components.app_bars.TopAppScrollBehavior
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class SmallTopBarViewModel : ViewModel() {
-    private val _uiState = MutableStateFlow(UiState())
-    val uiState: StateFlow<UiState> = _uiState.asStateFlow()
+    private val _uiState = MutableStateFlow(TopAppBarUiState())
+    val uiState: StateFlow<TopAppBarUiState> = _uiState.asStateFlow()
 
     fun updateAppBarTitle(title: String) {
         _uiState.value = _uiState.value.copy(appBarTitle = title)
@@ -38,13 +39,4 @@ class SmallTopBarViewModel : ViewModel() {
     fun getFruits(): List<String> {
         return MaterialDemoLocalRepository.getFruitList()
     }
-
-    data class UiState(
-        val appBarTitle: String = "Small Top App Bar",
-        val isShowNavigationIcon: Boolean = false,
-        val isShowBarEndActions: Boolean = false,
-        val appBarScrollBehavior: TopAppScrollBehavior = TopAppScrollBehavior.None,
-        val windowInsetsRect: Rect = Rect(0f, 0f, 0f, 0f),
-        val isUseCustomWindowInsets: Boolean = false,
-    )
 }
