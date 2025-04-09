bf40393448ad473e5fcaf6e1c172341f3e698625
HuangZhaozhuo <q1548701641@gmail.com>
2025-04-09 23:54:59 +0800

    feat: add RoundedPolygon vertices control
    This commit introduces the ability to control the number of vertices in the RoundedPolygon shape. A slider has been added to the BasicShapeScreen, allowing users to adjust the number of vertices in the displayed shape. The number of vertices can be adjusted between 3 and 10.

--- libs/compose_shape/src/main/java/com/example/compose_shape/ui/BasicShapeScreen.kt
+++ libs/compose_shape/src/main/java/com/example/compose_shape/ui/BasicShapeScreen.kt
@@ -3,17 +3,27 @@ package com.example.compose_shape.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Slider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.unit.dp
 import androidx.graphics.shapes.RoundedPolygon
 import androidx.graphics.shapes.toPath
 
 @Composable
 fun BasicShapeScreen(modifier: Modifier = Modifier) {
+    val maxVertices = 10
+    val minVertices = 3
+    var vertices by remember { mutableIntStateOf(minVertices) }
     Box(
         modifier = modifier
             .fillMaxSize()
@@ -25,7 +35,7 @@ fun BasicShapeScreen(modifier: Modifier = Modifier) {
                 .fillMaxSize()
                 .drawWithCache {
                     val roundedPolygon = RoundedPolygon(
-                        numVertices = 6,
+                        numVertices = vertices,
                         radius = size.minDimension / 2,
                         centerX = size.width / 2,
                         centerY = size.height / 2
@@ -36,6 +46,14 @@ fun BasicShapeScreen(modifier: Modifier = Modifier) {
                     }
                 }
                 .fillMaxSize()
+        )
+        Slider(
+            value = vertices.toFloat(),
+            valueRange = minVertices.toFloat()..maxVertices.toFloat(),
+            onValueChange = { vertices = it.toInt() },
+            modifier = Modifier
+                .padding(bottom = 24.dp)
+                .align(Alignment.BottomCenter)
         )
     }
 }
